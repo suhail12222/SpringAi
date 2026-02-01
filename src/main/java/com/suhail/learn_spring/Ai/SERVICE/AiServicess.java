e19321ceaaa025dfec20676e7863cc94b7c134e8
Suhail <muhmmadasifmalik593@gmail.com>
2026-02-01 08:49:59 +0530

    adding ai service converting text into embeddings numeric values by suhail

--- src/main/java/com/suhail/learn_spring/Ai/SERVICE/AiServicess.java
+++ src/main/java/com/suhail/learn_spring/Ai/SERVICE/AiServicess.java
@@ -1,17 +1,22 @@
 package com.suhail.learn_spring.Ai.SERVICE;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class AiServicess {
 
     private final ChatClient chatClient;
     private final EmbeddingModel embeddingModel;
-public float[]getEmbedding(String text){
+
+    public AiServicess(ChatClient chatClient, EmbeddingModel embeddingModel) {
+        this.chatClient = chatClient;
+        this.embeddingModel = embeddingModel;
+    }
+
+    public float[]getEmbedding(String text){
     return embeddingModel.embed(text);//array of float means converting text into numeric values
 }
 
